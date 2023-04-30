567d592d4bceee49ffff952e35712f615a5f8e88
cashieeetime <cashelcreates@gmail.com>
2023-04-30 11:09:22 -0400

    buttons pt2

--- src/App.js
+++ src/App.js
@@ -5,27 +5,20 @@ import ReactDOM from 'react-dom/client';
 
 
 function MyForm() {
-  const [ip, setIP] = useState("");
-  const [city, setCity] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <form>
-          <button>What's my IP address?</button><br></br>
-          <button>What's my city?</button><br></br>
+        <button>What's my IP address?</button><br></br>
+        <button>What's my city?</button><br></br>
           
-        </form>
+      
 
       </header>
     </div>
   )
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
-
 
 function App() {
   return MyForm()
