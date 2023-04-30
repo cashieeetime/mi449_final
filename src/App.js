661ed88bbea4b64d9996514ea6326c35bcb9430d
cashieeetime <cashelcreates@gmail.com>
2023-04-30 12:20:53 -0400

    tesing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Button1() {
     fetch('https://api.ipify.org')
           // .then((response) => response.json())
           .then((data) => {
-            alert(data.text);
+            alert(data.text());
           }) 
           .catch((err) => {
             console.log(err.message);
