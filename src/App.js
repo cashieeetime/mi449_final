28f25ed2a7fbaa045be803af982433a4223d92ca
cashieeetime <cashelcreates@gmail.com>
2023-04-30 11:12:50 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-
 
 function MyForm() {
-
   return (
     <div className="App">
       <header className="App-header">
 
         <button>What's my IP address?</button><br></br>
         <button>What's my city?</button><br></br>
-          
-      
-
+        
       </header>
     </div>
   )
@@ -22,6 +16,6 @@ function MyForm() {
 
 function App() {
   return MyForm()
-}
+} 
 
 export default App;
