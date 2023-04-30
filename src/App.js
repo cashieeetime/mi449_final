956afe6250ee90448eed49d9734c530f90e842f1
cashieeetime <cashelcreates@gmail.com>
2023-04-30 08:22:45 -0400

    testing jsx forms

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form>
-          <label for="fname">First name:</label><br>
-          <input type="text" id="fname" name="fname"><br>
-          <label for="lname">Last name:</label><br>
-          <input type="text" id="lname" name="lname">
+          <label>Enter your name:
+            <input type="text" />
+          </label>
         </form>
       </header>
     </div>
   );
 }
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
 export default App;
