83251e3d339ef01df6ddda2ed96fa3fb96d26977
cashieeetime <cashelcreates@gmail.com>
2023-04-30 09:08:29 -0400

    submit pop up

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import ReactDOM from 'react-dom/client';
 
 function MyForm() {
   const [name, setName] = useState("");
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
   return (
     <div className="App">
       <header className="App-header">
 
-        <form>
-          <label>What song are you looking for?: <br></br>
-            <input
-              type="text" 
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label><br></br>
-          <input type="submit" />
-        </form>
-
       </header>
+
+      <form handleSubmit >
+        <label>What song are you looking for?: <br></br>
+          <input
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label><br></br>
+        <input type="submit" />
+      </form>
+
     </div>
   )
 }
