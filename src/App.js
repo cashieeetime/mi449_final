f4114427a79db85564ec040dba9e7e6db8396741
cashieeetime <cashelcreates@gmail.com>
2023-04-30 15:07:37 -0400

    testing new api

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ function Mypage () {
   }  
 
   function handleClickB2() {
-    fetch('https://geoip.maxmind.com/geoip/v2.1/city/99.6.61.195?use-downloadable-db=1&demo=1&lang=en', {headers: {Authentication: 'Bearer v2.local.zfVRoOkNURXQhUD4CyzJLldEiDvU1LTwOoEMw42J9jee3UR7ch0LAtEbJs27Weub9GgkXe839l8P36Un-Eh7rYajXXOOPpnRqMricl2Bhq_ExjzU7yWu35OJZwDJ-tlp3N0tj3KGBJx2frBDsAgxw0Ybmfh3U0D8yJvVKDJiMXXOeshtaQmsQX6mIvFru3yZE0oslqsojFEq50hh'}})
-          .then((response) => response.json())
-          .then((data) => {
-            // data = data[ipAddress[5]];
-            alert(JSON.stringify(data));
-          }) 
-          .catch((err) => {
-            console.log(err.message);
-          });
+    fetch('https://ipapi.co/8.8.8.8/json/')
+      .then(function(response) {
+      response.json().then(jsonData => {
+      console.log(jsonData);
+    });
+  })
+  .catch(function(error) {
+  console.log(error)
+  });
   }
 
   return (
