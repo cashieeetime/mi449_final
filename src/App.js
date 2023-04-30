eca07c00fec279aebb5fdc7135bdd2a3e8013fe7
cashieeetime <cashelcreates@gmail.com>
2023-04-30 14:45:51 -0400

    format

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 // var ipAddress = "";
 
-function Button1() {
-  function handleClick() {
+function Mypage () {
+  function handleClickB1() {
     fetch('https://api.ipify.org?format=json')
           .then((response) => response.json())
           .then((data) => {
@@ -15,21 +15,9 @@ function Button1() {
           .catch((err) => {
             console.log(err.message);
           });
-  }
+  }  
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleClick}>
-          What's my IP address?
-        </button>
-      </header>
-    </div>
-  )  
-}
-
-function Button2() {
-  function handleClick() {
+  function handleClickB2() {
     fetch('https://geoip.maxmind.com/geoip/v2.1/city/99.6.61.195?use-downloadable-db=1&demo=1&lang=en', {headers: {Authentication: 'Bearer v2.local.zfVRoOkNURXQhUD4CyzJLldEiDvU1LTwOoEMw42J9jee3UR7ch0LAtEbJs27Weub9GgkXe839l8P36Un-Eh7rYajXXOOPpnRqMricl2Bhq_ExjzU7yWu35OJZwDJ-tlp3N0tj3KGBJx2frBDsAgxw0Ybmfh3U0D8yJvVKDJiMXXOeshtaQmsQX6mIvFru3yZE0oslqsojFEq50hh'}})
           .then((response) => response.json())
           .then((data) => {
@@ -42,19 +30,29 @@ function Button2() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleClick}>
-          What's my City?
-        </button>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={handleClickB1}>
+            What's my IP address?
+          </button>
+        </header>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <button onClick={handleClickB2}>
+            What's my City?
+          </button>
+        </header>
+      </div>
     </div>
-  )  
+  )
 }
 
 function App() {
   return (
-    Button1(), Button2()
+    Mypage()
   )
 } 
 
