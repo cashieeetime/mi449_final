1f5d1b84ef68bbabac994bdf6cf4bfc823f007c4
cashieeetime <cashelcreates@gmail.com>
2023-04-30 14:08:25 -0400

    testing second button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function Button1() {
           .then((response) => response.json())
           .then((data) => {
             alert(JSON.stringify(data));
+            const ipAddress = data;
           }) 
           .catch((err) => {
             console.log(err.message);
@@ -20,15 +21,39 @@ function Button1() {
         <button onClick={handleClick}>
           What's my IP address?
         </button>
-        
+      </header>
+    </div>
+  )  
+}
 
+function Button2() {
+  function handleClick() {
+    fetch('https://geoip.maxmind.com/geoip/v2.1/city/99.6.61.195\?use-downloadable-db=1\&demo=1\&lang=en')
+          .then((response) => response.json())
+          .then((data) => {
+            ip = ipAddress[1];
+            alert(JSON.stringify(data));
+          }) 
+          .catch((err) => {
+            console.log(err.message);
+          });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleClick}>
+          What's my City?
+        </button>
       </header>
     </div>
   )  
 }
 
 function App() {
-  return Button1()
+  return (
+    Button1(), Button2()
+  )
 } 
 
 export default App;
